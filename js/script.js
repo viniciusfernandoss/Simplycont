1aa1903d63425d8ab6c0ebee927c0230a7e9721a
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-11-19 09:45:48 -0300

    Trabalho 19/11/2024

--- js/script.js
+++ js/script.js
@@ -41,3 +41,15 @@ const swiper = new Swiper('.swiper-feedback', {
       }
   }
 });
+
+
+
+//Navbar
+window.onscroll = function() {
+    var containerH = document.getElementById('headerr');
+    if (window.scrollY > 1) { // Ajuste este valor conforme necessário
+        containerH.style.display = 'none';
+    } else {
+        containerH.style.display = 'block';
+    }
+};
